4873ac7ef11def7d5a554ace1f1c38c7b441883c
Ryan Soklaski <ry26099@mit.edu>
2019-12-27 14:23:00 -0500

    parameterize tests

--- tests/test_basic_functions.py
+++ tests/test_basic_functions.py
@@ -1,37 +1,23 @@
+import pytest
 from plymi_mod6.basic_functions import count_vowels, merge_max_mappings
 
 
-def test_count_vowels_basic():
-    # test basic strings with uppercase and lowercase letters
-    assert count_vowels("aA bB yY", include_y=False) == 2
-    assert count_vowels("aA bB yY", include_y=True) == 4
-
-    # test empty strings
-    assert count_vowels("", include_y=False) == 0
-    assert count_vowels("", include_y=True) == 0
+@pytest.mark.parametrize(
+    ("input_string", "include_y", "expected"),
+    [("aA bB yY", False, 2), ("aA bB yY", True, 4), ("", False, 0), ("", True, 0)],
+)
+def test_count_vowels_basic(input_string: str, include_y: bool, expected: int):
+    assert count_vowels(input_string, include_y) == expected
 
 
-def test_merge_max_mappings():
-    # test documented behavior
-    dict1 = {"a": 1, "b": 2}
-    dict2 = {"b": 20, "c": -1}
-    expected = {'a': 1, 'b': 20, 'c': -1}
-    assert merge_max_mappings(dict1, dict2) == expected
-
-    # test empty dict1
-    dict1 = {}
-    dict2 = {"a": 10.2, "f": -1.0}
-    expected = dict2
-    assert merge_max_mappings(dict1, dict2) == expected
-
-    # test empty dict2
-    dict1 = {"a": 10.2, "f": -1.0}
-    dict2 = {}
-    expected = dict1
-    assert merge_max_mappings(dict1, dict2) == expected
-
-    # test both empty
-    dict1 = {}
-    dict2 = {}
-    expected = {}
+@pytest.mark.parametrize(
+    ("dict1", "dict2", "expected"),
+    [
+        (dict(a=1, b=2), dict(b=20, c=-1), dict(a=1, b=20, c=-1)),
+        (dict(), dict(b=20, c=-1), dict(b=20, c=-1)),
+        (dict(a=1, b=2), dict(), dict(a=1, b=2)),
+        (dict(), dict(), dict()),
+    ],
+)
+def test_merge_max_mappings(dict1: dict, dict2: dict, expected: dict):
     assert merge_max_mappings(dict1, dict2) == expected
