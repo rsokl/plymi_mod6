5c35594bbdbcfa3192d9d8bf692d16708a918036
Ryan Soklaski <ry26099@mit.edu>
2019-12-28 12:55:07 -0500

    rename parameters

--- tests/test_basic_functions.py
+++ tests/test_basic_functions.py
@@ -3,15 +3,15 @@ from plymi_mod6.basic_functions import count_vowels, merge_max_mappings
 
 
 @pytest.mark.parametrize(
-    ("input_string", "include_y", "expected"),
+    "input_string, include_y, expected_count",
     [("aA bB yY", False, 2), ("aA bB yY", True, 4), ("", False, 0), ("", True, 0)],
 )
-def test_count_vowels_basic(input_string: str, include_y: bool, expected: int):
-    assert count_vowels(input_string, include_y) == expected
+def test_count_vowels_basic(input_string: str, include_y: bool, expected_count: int):
+    assert count_vowels(input_string, include_y) == expected_count
 
 
 @pytest.mark.parametrize(
-    ("dict1", "dict2", "expected"),
+    "dict_a, dict_b, expected_merged",
     [
         (dict(a=1, b=2), dict(b=20, c=-1), dict(a=1, b=20, c=-1)),
         (dict(), dict(b=20, c=-1), dict(b=20, c=-1)),
@@ -19,5 +19,5 @@ def test_count_vowels_basic(input_string: str, include_y: bool, expected: int):
         (dict(), dict(), dict()),
     ],
 )
-def test_merge_max_mappings(dict1: dict, dict2: dict, expected: dict):
-    assert merge_max_mappings(dict1, dict2) == expected
+def test_merge_max_mappings(dict_a: dict, dict_b: dict, expected_merged: dict):
+    assert merge_max_mappings(dict_a, dict_b) == expected_merged
